5a62bf6777ed3c0a815efe7e0ac29a53d583709a
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-04 18:53:56 +0300

    generate associations and attributes for review

--- db/migrate/20221204154314_create_reviews.rb
+++ db/migrate/20221204154314_create_reviews.rb
@@ -1,6 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
+      t.belongs_to :user
+      t.belongs_to :product
+      t.string :message
 
       t.timestamps
     end
