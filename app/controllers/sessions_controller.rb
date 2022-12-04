48b23f1ebd1ec5f9f7099a05ca251cf8bcab355b
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 01:18:24 +0300

    add post request for user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,13 @@
 class SessionsController < ApplicationController
   def index
-    session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    session[:session_hello] ||= "Mokua"
+    cookies[:cookies_hello] ||= "Mokua"
     render json: { session: session, cookies: cookies.to_hash }
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    session[user_id] = user.id
+    render json: user
   end
 end
