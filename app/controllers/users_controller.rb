d85aa34e41de586582fe8540088b6fda6b165041
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 02:09:18 +0300

    create new show method for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  # def show
+  #   user = User.find(params[:id])
+  #   render json: user
+  # end
   def show
-    user = User.find(params[:id])
-    render json: user
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
   end
 
   private
