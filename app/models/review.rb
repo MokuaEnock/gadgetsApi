3cf2ec173c2aa9fcf20704331e9b0f0c96490e0a
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-04 21:29:54 +0300

    add validations for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ApplicationRecord
+  validates :message, presence: true
+  validates :rating,
+            presence: true,
+            numericality: {
+              greater_than_or_equal_to: 1,
+              less_than_or_equal_to: 5
+            }
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+
   belongs_to :user
   belongs_to :product
 end
