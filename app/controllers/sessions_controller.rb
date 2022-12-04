a2e0a96445b4848622adaf2665e1bcd33e3b734a
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 00:25:52 +0300

    add sample cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
 
   def click
-    
+    cookies[:click] ||= 0
+    cookies[:click] += cookies[:click].to_i
+    cookies[:click] ||= 0
+    session[:click] += 1
   end
 end
