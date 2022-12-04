9230e3e52c8558dcb2c86ef919b2d760aa036f24
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-04 23:48:31 +0300

    create session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,8 @@
 class SessionController < ApplicationController
+
+  def click
+    cookies[:click] ||= 0
+    cookie[:click] += 1
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :reviews, only: %i[show index]
   end
 
+  get "/cookie_click", to: "session/click"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
