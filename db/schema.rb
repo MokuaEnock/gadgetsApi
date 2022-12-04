146950c4bb3884754e8d3df63bbf7a430b44fa24
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-04 19:06:44 +0300

    change in table schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_154314) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_160556) do
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "price"
     t.string "description"
     t.string "category"
     t.string "image"
