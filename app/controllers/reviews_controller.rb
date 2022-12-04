5b6c1b83d2429347d359be65f33b4de116a5f115
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-04 20:58:32 +0300

    create error boundaries for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid,
+              with: :render_unprocessable_entity_response
 
   def index
     reviews = Review.all
@@ -11,13 +13,25 @@ class ReviewsController < ApplicationController
     render json: review
   end
 
+  def create
+    review = Review.create!(review_params)
+    render json: review
+  end
+
   private
 
   def render_not_found_response
     render json: { error: "Review not found" }, status: :not_found
   end
 
-  def params
+  def review_params
     params.permit(:message, :rating, :user_id, :product_id)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {
+             error: invalid.record.errors.full_messages
+           },
+           status: :unprocessable_entity
   end
 end
