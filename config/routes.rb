07918e7d4736b6fdf535f00f1f2b047aad9d9472
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-04 22:14:20 +0300

    remove custome resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :products
 
-  get "/products/:product_id/reviews", to: "products#reviews_index"
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
