a9d4d16ad4f5b491a2dd7d658de916231ffe72cb
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-04 23:05:29 +0300

    add Action Dispatch

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,10 @@ module GadgetsApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.api_only = true
-    config.middleware.use ActionDisptch::Cookies
-    config.middleware.use ActionDisptch::Session::CookieStore
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
 
-    config.action_dispatch.cookies_same_site_protectin = :strict 
+    config.action_dispatch.cookies_same_site_protectin = :strict
 
     # Configuration for the application, engines, and railties goes here.
     #
