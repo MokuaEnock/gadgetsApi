194324e3063a87bf2fa4c9f68f8cf225aa76f420
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 03:17:23 +0300

    create authoriz method for paplication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+
+  private
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    unless @current_user
+      render json: { errors: ["Not authorized"] }, status: :unauthorized
+    end
+  end
 end
