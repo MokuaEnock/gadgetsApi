e54f718df431e5278e70a44b2bbd07718f57897c
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 03:20:19 +0300

    remove redudant functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,4 @@
 class UsersController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
-  # def show
-  #   user = User.find(params[:id])
-  #   render json: user
-  # end
   def show
     user = User.find_by(id: session[:user_id])
     if user
@@ -12,13 +6,5 @@ class UsersController < ApplicationController
     else
       render json: { error: "Not authorized" }, status: :unauthorized
     end
-  end
-
-
-
-  private
-
-  def render_not_found_response
-    render json: { error: "User not found" }, status: :not_found
   end
 end
