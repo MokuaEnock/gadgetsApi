2a5ddab2b9fd7d7e7222cfa7f6c477384b0bf602
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 03:23:31 +0300

    add create methods for create and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def create
+    user = user.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
   def show
-    user = User.find_by(id: session[:user_id])
-    if user
-      render json: user
-    else
-      render json: { error: "Not authorized" }, status: :unauthorized
-    end
+    render json: @current_user
   end
 end
